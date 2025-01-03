d5d6bc8da42704c72414a5ee8f52828aaf7aeb9b
funZ <153361103+PaFunzFr@users.noreply.github.com>
2025-01-03 17:31:14 +0100

    Update 250103

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-let noteTitle = document.getElementById('noteTitle');
-let noteText = document.getElementById('noteText');
-let noteContainerCurrent = document.getElementById('notesContainer');
+let noteTitle = document.getElementById("noteTitle");
+let noteText = document.getElementById("noteText");
+let noteContainerCurrent = document.getElementById("notesContainer");
+let noteContainerArchived = document.getElementById("notesContainerAchived");
+
+let date = new Date();
+const day = date.getDate();
+const month = date.getMonth() + 1;
+const year = date.getFullYear();
+const hours = date.getHours();
+const minutes = date.getMinutes();
+const time = `${hours}:${minutes}`;
 
 currentNotes = [];
 currentNotesText = [];
@@ -9,35 +18,58 @@ archivedNotesText = [];
 deletedNotes = [];
 deletedNotesText = [];
 
+function renderInit() {
+  renderNotes(currentNotes, currentNotesText, noteContainerCurrent);
+  renderNotes(archivedNotes, archivedNotesText, noteContainerArchived);
+}
+
+// add notes to current notes
 function addNote() {
-    currentNotes.push(noteTitle.value);
-    currentNotesText.push(noteText.value);
-    renderNotes(currentNotes, currentNotesText);
+  currentNotes.push(noteTitle.value);
+  currentNotesText.push(noteText.value);
+  renderNotes(currentNotes, currentNotesText, noteContainerCurrent);
+  noteTitle.value = "";
+  noteText.value = "";
 }
 
-
-let date = new Date();
-const day = date.getDate();
-const month = date.getMonth() +1;
-const year = date.getFullYear();
-const hours = date.getHours();
-const minutes = date.getMinutes();
-
-const time = `${hours}:${minutes}`;
-
-function renderNotes(array1, array2) {
-    noteContainerCurrent.innerHTML = '';
-    for (i = 0; i < currentNotes.length; i++) {
-        noteContainerCurrent.innerHTML += `
-            <div class="note" id="singleNote">
-            <h3 class="note-title">${array1[i]}<br>posted on  
-            <span class="date">${day}.${month}.${year}</span> | 
-            <span class="time">${time}</h3>
-            <p class="note-text">${array2[i]}</p>
-            <div class="h-line"></div>
-            <button>EDIT</button>
-            <button>X</button>
-            </div>
-        `
-        };
+// render current notes
+function renderNotes(array1, array2, containerToRender) {
+    containerToRender.innerHTML = "";
+    for (i = array1.length -1; i > -1; i--) {
+        containerToRender.innerHTML += `
+                <div class="note" id="singleNote">
+                    <h3 class="note-title">${array1[i]}<br> 
+                    <span class="date">posted on ${day}.${month}.${year} | ${time}</h3>
+                    <p class="note-text">${array2[i]}</p>
+                    <div class="h-line"></div>
+                    ${noteButtons(i)}
+                </div>`;
+    }
 }
+// button template
+function noteButtons(i) {
+  return `
+        <button onclick="editNote()">EDIT</button>
+        <button onclick="moveToArchive(${i})">DONE</button>
+        <button onclick="moveToBin()">X</button>`;
+};
+
+function moveToArchive(index) {
+    renderNotesArchived(index);
+    renderInit();
+  };
+  
+// is only executed when there are existing entries in currentNotes (!= undefined)
+function renderNotesArchived(index) {
+  if (currentNotes[index] != undefined) { // multiple clicks were possible with undefined text in it
+    archivedNotes.push(currentNotes[index]);
+    archivedNotesText.push(currentNotesText[index]);
+    currentNotes.splice(index, 1);
+    currentNotesText.splice(index, 1);
+    renderInit();
+  }
+}
+
+function moveToBin() {}
+
+function editNote() {}
