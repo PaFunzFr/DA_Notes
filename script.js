136b9ed2f480153ade0ff20c41e837bbc5608a6c
funZ <153361103+PaFunzFr@users.noreply.github.com>
2025-01-07 15:01:36 +0100

    Update 250107

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 // DOM Elements
 let noteTitle = document.getElementById("noteTitle");
 let noteText = document.getElementById("noteText");
-let currentNotesRef = document.getElementById("notesContainer");
-let archivedNotesRef = document.getElementById("notesContainerArchived");
+let currentNotesRef = document.getElementById("current");
+let archivedNotesRef = document.getElementById("archived");
+let deletedNotesRef = document.getElementById("deleted");
 
 // configuring time stamp
 let date = new Date();
@@ -14,21 +15,24 @@ const minutes = date.getMinutes();
 const time = `${hours}:${String(minutes).padStart(2, '0')}`;
 const timeStamp = `${day}.${month}.${year} | ${time}`;
 
-// Load notes from localStorage or initialize empty arrays
+// load notes from localStorage / initialize empty arrays
 let currentNotes = JSON.parse(localStorage.getItem(`${currentNotesRef.id} Title:`)) || [];
 let currentNotesText = JSON.parse(localStorage.getItem(`${currentNotesRef.id} Text:`)) || [];
 let datePosted = JSON.parse(localStorage.getItem(`${currentNotesRef.id} Date:`)) || [];
 let archivedNotes = JSON.parse(localStorage.getItem(`${archivedNotesRef.id} Text:`)) || [];
 let archivedNotesText = JSON.parse(localStorage.getItem(`${archivedNotesRef.id} Text:`)) || [];
+let deletedNotes = JSON.parse(localStorage.getItem(`${deletedNotesRef.id} Text:`)) || [];
+let deletedNotesText = JSON.parse(localStorage.getItem(`${deletedNotesRef.id} Text:`)) || [];
 
 function renderInit() {
   renderNotes(currentNotesRef, currentNotes, currentNotesText);
   renderNotes(archivedNotesRef, archivedNotes, archivedNotesText);
+  renderNotes(deletedNotesRef, deletedNotes, deletedNotesText);
 }
 
-// Create new note, push to array and save to localStorage
+// create new note, push to array and save to localStorage
 function addNewNote() {
-  if (noteTitle.value != "" || noteText.value != "") {
+  if (noteTitle.value != "" && noteText.value != "") {
     currentNotes.push(noteTitle.value);
     currentNotesText.push(noteText.value);
     datePosted.push(timeStamp);
@@ -45,14 +49,19 @@ function saveToLocalStorage(refContainer, title, text) {
   localStorage.setItem(`${refContainer.id} Date:`, JSON.stringify(datePosted));
 }
 
+function updateLocalStorage() {
+  saveToLocalStorage(currentNotesRef, currentNotes, currentNotesText);
+  saveToLocalStorage(archivedNotesRef, archivedNotes, archivedNotesText);
+  saveToLocalStorage(deletedNotesRef, deletedNotes, deletedNotesText);
+}
 // Render current notes
 function renderNotes(containerToRender, title, text) {
-  containerToRender.innerHTML = "";
+  containerToRender.innerHTML = ``;
   noteTemplate(containerToRender, title, text, datePosted);
 }
 
 function noteTemplate(containerToRender, title, text, date) {
-  if (title && title.length > 0 && text && text.length > 0 && date && date.length > 0) {
+  if (title.length && text.length && date.length) {
     for (let i = title.length - 1; i > -1; i--) {
       containerToRender.innerHTML += `
         <div class="note" id="singleNote">
@@ -60,40 +69,82 @@ function noteTemplate(containerToRender, title, text, date) {
             <span class="date">posted on ${date[i]}</h3>
             <p class="note-text">${text[i]}</p>
             <div class="h-line"></div>
-            <button class="btn-${containerToRender.id}-arc" onclick="moveToArchive(${i})">DONE</button>
-            <button class="btn-${containerToRender.id}-del" onclick="moveToBin()">X</button>
+            <button class="btn-${containerToRender.id}-cur" onclick="moveToCurrent(event, ${i})">make current</button>
+            <button class="btn-${containerToRender.id}-arc" onclick="moveToArchive(event, ${i})">to archive</button>
+            <button class="btn-${containerToRender.id}-del" onclick="moveToBin(event, ${i})">X</button>
         </div>`;
-        checkButtonId(containerToRender);
+        checkCurButtons(containerToRender);
+        checkArcButtons(containerToRender);
     }
   }
 }
 
-function moveToArchive(index) {
-    let clickedArchiveNote = currentNotes.splice(index, 1)[0];
-    let clickedArchiveNoteText = currentNotesText.splice(index, 1)[0];
-    archivedNotes.push(clickedArchiveNote);
-    archivedNotesText.push(clickedArchiveNoteText);
-    saveToLocalStorage(currentNotesRef, currentNotes, currentNotesText);
-    saveToLocalStorage(archivedNotesRef, archivedNotes, archivedNotesText);
-    renderInit();
+function moveToCurrent(event, index) {
+  if (event.target.className.includes("archived")) {
+    moveNote(archivedNotes, archivedNotesText, index, currentNotes, currentNotesText);
+  }
+  if (event.target.className.includes("deleted")) {
+    moveNote(deletedNotes, deletedNotesText, index, currentNotes, currentNotesText)
+  }
 }
 
+function moveToArchive(event, index) {
+  if (event.target.className.includes("current")) {
+    moveNote(currentNotes, currentNotesText, index, archivedNotes, archivedNotesText);
+  }
+  if (event.target.className.includes("deleted")) {
+    moveNote(deletedNotes, deletedNotesText, index, archivedNotes, archivedNotesText);
+  }
+}
 
-function checkButtonId(buttonRef) {
-  let buttonElement = document.getElementsByClassName(`btn-${buttonRef.id}-arc`);
-  for (let i = 0; i < buttonElement.length; i++) {
-    if(buttonElement) {
-      if (buttonElement[i].className.includes("Archived")) {
-        buttonElement[i].style.display = 'none';
-        console.log("Archived");
+function moveToBin(event, index) {
+  if (event.target.className.includes("current")) {
+    moveNote(currentNotes, currentNotesText, index, deletedNotes, deletedNotesText);
+  }
+  if (event.target.className.includes("archived")) {
+    moveNote(archivedNotes, archivedNotesText, index, deletedNotes, deletedNotesText);
+  }
+  if (event.target.className.includes("deleted")) {
+    deleteNote(deletedNotes, deletedNotesText, index)
+  }
+}
+
+function moveNote(arrayRef1, arrayRef2, index, arrayTarget1, arrayTarget2) {
+  let clickedNoteTitle = arrayRef1.splice(index, 1)[0];
+  let clickedNoteText = arrayRef2.splice(index, 1)[0];
+  arrayTarget1.push(clickedNoteTitle);
+  arrayTarget2.push(clickedNoteText);
+  updateLocalStorage()
+  renderInit();
+}
+
+function deleteNote(arrayRef1, arrayRef2, index) {
+  arrayRef1.splice(index, 1)[0];
+  arrayRef2.splice(index, 1)[0];
+  updateLocalStorage()
+  renderInit();
+}
+
+function checkArcButtons(buttonRef) {
+  let buttonElementArc = document.getElementsByClassName(`btn-${buttonRef.id}-arc`);
+  for (let i = 0; i < buttonElementArc.length; i++) {
+    if(buttonElementArc) {
+      if (buttonElementArc[i].className.includes("archived")) {
+        buttonElementArc[i].style.display = 'none';
       }
     }
   }
 }
 
-function moveToBin() {}
-
-function editNote() {}
-
+function checkCurButtons(buttonRef) {
+  let buttonElementArc = document.getElementsByClassName(`btn-${buttonRef.id}-cur`);
+  for (let i = 0; i < buttonElementArc.length; i++) {
+    if(buttonElementArc) {
+      if (buttonElementArc[i].className.includes("current")) {
+        buttonElementArc[i].style.display = 'none';
+      }
+    }
+  }
+}
 // Initialize the rendering process
 renderInit();
